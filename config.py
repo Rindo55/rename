c544dec15f72ab710095120a595f98aeffe86346
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-17 00:41:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = "3845818"
 API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
 BOT_TOKEN = "6284581524:AAEHd9Idbj2sk5bARTkXX1CS_dGOXOgXuy4"
-ADMIN = "1443454117"         
+ADMIN = "1863307059"         
 CAPTION = "{file_name}"
 
 # for thumbnail ( back end is MrMKN brain 😉)
